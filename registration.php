1bdc61c625d1b9ef442d4d0b4cc709c02caeeae4
Sandro Wagner <sw@integer-net.de>
2020-09-11 15:03:58 +0200

    Replace placeholders

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    ':module-namespace_:module-name',
+    'IntegerNet_Config2rest',
     __DIR__ . '/src'
 );
